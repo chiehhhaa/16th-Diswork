ab82e0f38df4c80f279ad9136ccebfd9a90cd17d
stephan9971 <tom9876555@yahoo.com.tw>
2024-05-07 19:51:05 +0800

    modify: 修改 task/models.py model 欄位

--- tasks/models.py
+++ tasks/models.py
@@ -1,27 +1,34 @@
+from typing import Any
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+class TaskManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted_at=None)
+
 class Task(models.Model):
-    situation=models.ForeignKey(
-        'Situation',
-        on_delete=models.CASCADE
-    )
-    created_user_name=models.CharField(max_length=20)
-    title=models.CharField(max_length=20)
-    content=models.TextField(default='')
-    created_at=models.DateTimeField(auto_now_add=True)
-    start_time=models.DateTimeField(null=True)
-    end_time=models.DateTimeField(null=True)
+
+    created_user = models.CharField(max_length=20)
+    title = models.CharField(max_length=20)
+    content = models.TextField(default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    get_time = models.DateTimeField(null=True)
+    start_time = models.DateTimeField(null=True)
+    end_time = models.DateTimeField(null=True)
+    deleted_at = models.DateTimeField(null=True)
+    level = models.CharField(max_length=1, default='')
+
+    objects = TaskManager()
+
+    def delete(self):
+        self.deleted_at = timezone.now()
+        self.save()
 
 class TakeTask(models.Model):
-    task=models.ForeignKey(
-        'Task',
-        on_delete=models.CASCADE
-    )
+    pass
 
 class Situation(models.Model):
-    name=models.CharField(max_length=20)
+    col_name=models.CharField(max_length=20, default='')
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
