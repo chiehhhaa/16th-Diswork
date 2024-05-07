ee671c45406ae4ee97e8bdd8b6f178a8ff757060
stephan9971 <tom9876555@yahoo.com.tw>
2024-05-07 19:54:34 +0800

    modify: 修改 tasks/views.py 所以路由

--- tasks/views.py
+++ tasks/views.py
@@ -1,19 +1,29 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404, redirect, get_list_or_404
 from .models import Task
+from random import randint, choice
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 
 def index(req):
-    return HttpResponse('index')
+    tasks = get_list_or_404(Task)
+    
+    return render(req, 'tasks/index.html', {'tasks':tasks})
 
 def new(req):
-    return HttpResponse('new')
+    return render(req, 'tasks/new.html')
 
+@require_POST
 def create(req):
+    name_list = ['joe', 'john', 'may', 'mary']
+    # print(req.POST['get_time'])
+    # print(req.POST['start_time'])
+    # print(req.POST['end_time'])
     task = Task(
-        created_user_name=req.POST['created_user_name'],
+        created_user=choice(name_list),
         title=req.POST['title'],
         content=req.POST['content'],
+        get_time=req.POST['get_time'],
         start_time=req.POST['start_time'],
         end_time=req.POST['end_time'],
     )
@@ -22,11 +32,30 @@ def create(req):
     return HttpResponse('created is ok', task)
 
 def show(req, pk):
-    return HttpResponse('show')
+    task = get_object_or_404(Task, pk=pk)
+
+    if req.method == 'POST':
+        task.title=req.POST['title']
+        task.content=req.POST['content']
+        task.get_time=req.POST['get_time']
+        task.start_time=req.POST['start_time']
+        task.end_time=req.POST['end_time']
+        task.save()
+
+        return redirect('tasks:show', pk=task.id)
+
+    return render(req, 'tasks/show.html', {'task':task})
 
 def edit(req, pk):
-    return HttpResponse('edit')
+    task = get_object_or_404(Task, pk=pk)
+   
+    return render(req, 'tasks/edit.html', {'task':task})
 
+
+@require_POST
 def delete(req, pk):
-    return HttpResponse('delete')
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+   
+    return HttpResponse('')
 
