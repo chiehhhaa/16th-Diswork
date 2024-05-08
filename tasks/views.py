7540c3a88c94fa22f7eb2555cec4c3ba1c9a2d8c
stephan9971 <tom9876555@yahoo.com.tw>
2024-05-08 16:01:37 +0800

    update: tasks/views.py 修改新增,編輯路由添加時結束時間不能比開始時間，接到時間訊息

--- tasks/views.py
+++ tasks/views.py
@@ -2,13 +2,7 @@ from django.shortcuts import render, HttpResponse, get_object_or_404, redirect, 
 from .models import Task
 from random import randint, choice
 from django.views.decorators.http import require_POST
-from datetime import datetime
-from django.utils import timezone
-
-def to_aware_datetime(time):
-    naive_datetime = datetime.strptime(time, "%Y-%m-%dT%H:%M")
-    aware_datetime = timezone.make_aware(naive_datetime, timezone.get_current_timezone())
-    return aware_datetime
+from lib.utils import to_aware_datetime, comparison_time
 
 def index(req):
     tasks = get_list_or_404(Task)
@@ -21,32 +15,39 @@ def new(req):
 @require_POST
 def create(req):
     name_list = ['joe', 'john', 'may', 'mary']
+    task_state = comparison_time(req.POST['get_time'], req.POST['start_time'], req.POST['end_time'])
+    if (task_state['state'] == "error"):
+        return HttpResponse(f"{task_state['state']}: {task_state['message']}")
+    else:
+        task = Task(
+            created_user=choice(name_list),
+            title=req.POST['title'],
+            content=req.POST['content'],
+            get_time=to_aware_datetime(req.POST['get_time']),
+            start_time=to_aware_datetime(req.POST['start_time']),
+            end_time=to_aware_datetime(req.POST['end_time']),
+        )
+        task.save()
 
-    task = Task(
-        created_user=choice(name_list),
-        title=req.POST['title'],
-        content=req.POST['content'],
-        get_time=to_aware_datetime(req.POST['get_time']),
-        start_time=to_aware_datetime(req.POST['start_time']),
-        end_time=to_aware_datetime(req.POST['end_time']),
-    )
-    task.save()
-
-    return HttpResponse('created is ok', task)
+        return HttpResponse(f"{task_state['state']}: {task_state['message']}", task)
 
 def show(req, pk):
     task = get_object_or_404(Task, pk=pk)
     if req.method == 'POST':
-
-        task.title=req.POST['title']
-        task.content=req.POST['content']
-        task.get_time=to_aware_datetime(req.POST['get_time'])
-        task.start_time=to_aware_datetime(req.POST['start_time'])
-        task.end_time=to_aware_datetime(req.POST['end_time'])
-        task.save()
-
-        return redirect('tasks:show', pk=task.id)
-
+        task_state = comparison_time(req.POST['get_time'], req.POST['start_time'], req.POST['end_time'])
+        
+        if (task_state['state'] == "error"):
+            return HttpResponse(f"{task_state['state']}: {task_state['message']}")
+        else:
+            task.title=req.POST['title']
+            task.content=req.POST['content']
+            task.get_time=to_aware_datetime(req.POST['get_time'])
+            task.start_time=to_aware_datetime(req.POST['start_time'])
+            task.end_time=to_aware_datetime(req.POST['end_time'])
+            task.save()
+    
+            return redirect('tasks:show', pk=task.id)
+    
     return render(req, 'tasks/show.html', {'task':task})
 
 def edit(req, pk):
