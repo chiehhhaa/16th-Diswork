7440fb5ec9a94a3964df2c2a3e863ff07d46d529
stephan9971 <tom9876555@yahoo.com.tw>
2024-05-09 12:58:55 +0800

    fix project/settings.py

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Task(models.Model):
+    situation=models.ForeignKey(
+        'Situation',
+        on_delete=models.CASCADE
+    )
+    created_user_name=models.CharField(max_length=20)
+    title=models.CharField(max_length=20)
+    content=models.TextField(default='')
+    created_at=models.DateTimeField(auto_now_add=True)
+    start_time=models.DateTimeField(null=True)
+    end_time=models.DateTimeField(null=True)
+
+class TakeTask(models.Model):
+    task=models.ForeignKey(
+        'Task',
+        on_delete=models.CASCADE
+    )
+
+class Situation(models.Model):
+    name=models.CharField(max_length=20)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
