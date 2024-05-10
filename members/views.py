314e3f642f1799659aa8a8c169049db9e42225f9
stephan9971 <tom9876555@yahoo.com.tw>
2024-05-10 12:51:48 +0800

    update 刪除第三方功能檔案多餘的程式碼

--- members/views.py
+++ members/views.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import SignUpForm
 from django.contrib.auth import login, logout
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.views.generic import TemplateView, FormView, UpdateView
 from django.urls import reverse_lazy
@@ -25,7 +24,6 @@ class LoginView(FormView):
     form_class = AuthenticationForm
     success_url = reverse_lazy("index")
 
-    # @login_required
     def form_valid(self, form):
         login(self.request, form.get_user())
         return super().form_valid(form)
