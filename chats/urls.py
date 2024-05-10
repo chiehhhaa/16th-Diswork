9d7eaf8f82b3404d5cffbbb74d618bfee0902927
ErJun Wu <defyto13579@gmail.com>
2024-05-10 18:16:46 +0800

    fit: rebase dev

--- chats/urls.py
+++ chats/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = "chats"
 
 urlpatterns = [
-    path("", views.chat_view, name="home"),
+    path('', views.chat_view_home, name = "home"),
+    path('new', views.chat_view_new, name = "new"),
+    path('add', views.chat_view_create, name = "add"),
+    path('chats/<int:id>/', views.chat_view_show, name='show'),
 ]
--- chats/views.py
+++ chats/views.py
@@ -3,13 +3,20 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import *
+from .models import ChatGroup
 from .forms import ChatmessageCreateForm
 
+def chat_view_home(request):
+    chat_groups = ChatGroup.objects.all()
+    return render(request, "chats/chat_home.html", {"chat_groups": chat_groups})
 
-@login_required
-def chat_view(request):
-    chat_group = get_object_or_404(ChatGroup, group_name="public-chat")
+def chat_view_new(request):
+    return render(request, "chats/chat_create.html")
+
+
+# @login_required
+def chat_view_show(request, id):
+    chat_group = get_object_or_404(ChatGroup, pk = id)
     chat_messages = chat_group.chat_messages.all()[:30]
     form = ChatmessageCreateForm()
 
@@ -20,7 +27,16 @@ def chat_view(request):
             message.author = request.user
             message.group = chat_group
             message.save()
-            return redirect("chats:home")
-    return render(
-        request, "chats/chat.html", {"chat_messages": chat_messages, "form": form}
-    )
+            # return redirect('chats:home')
+            return redirect('chats:show', id=chat_group.id)
+    return render(request, "chats/chat_group.html", {'chat_messages': chat_messages, 'form': form})
+
+def chat_view_create(request):
+    # chat_group_name = ChatGroup.objects.all()
+    if request.method == 'POST':
+        chat_group = ChatGroup(
+            group_name = request.POST['group_name']
+        )
+        chat_group.save()
+    return redirect('chats:home')
+
