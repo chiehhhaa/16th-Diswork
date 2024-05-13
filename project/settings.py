50ad0ef8b6a1b9aaf571b93b8d3eeb8733c5e891
chiehhhaa <tomato870208@gmail.com>
2024-05-13 11:04:16 +0800

    fix setting.py

--- project/settings.py
+++ project/settings.py
@@ -164,12 +164,6 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),  # 確保這個路徑指向的目錄存在
-# ]
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
