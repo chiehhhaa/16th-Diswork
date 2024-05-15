92dad1c93ac63ac1339175478bb434fa48a23be5
stephan9971 <tom9876555@yahoo.com.tw>
2024-05-15 18:05:24 +0800

    fix: 私人訊息 model 增加 group 欄位，chats/consumers.py 刪除註解

--- chats/models.py
+++ chats/models.py
@@ -20,8 +20,10 @@ class GroupMessage(models.Model):
         ordering = ['-created_at']
 
 class PrivateMessage(models.Model):
-    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='sent_messages')
-    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='received_messages')
+    group = models.ForeignKey(ChatGroup, on_delete = models.CASCADE, related_name = 'private_messages',
+    )
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name ='sent_messages')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name ='received_messages')
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     is_read = models.BooleanField(default=False)
