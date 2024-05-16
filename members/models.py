a4c253f7166faa4192c7c1bbbfeb72311ffddcb8
Jeter Chen <dali1756@gmail.com>
2024-05-16 11:56:26 +0800

    登入信箱驗證

--- members/models.py
+++ members/models.py
@@ -7,3 +7,4 @@ class Member(AbstractUser):
     vip_user = models.CharField(max_length=50, default="")
     vip_date_expiry = models.DateTimeField(null=True, blank=True)
     user_img = models.ImageField(null=True, blank=True)
+    email = models.EmailField(unique=True)
