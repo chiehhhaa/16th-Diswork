8a52ffe90f07e8d9ef6ec5b3985de731050d23e0
Jeter Chen <dali1756@gmail.com>
2024-05-17 16:02:29 +0800

    feat: taksk models manytomany

--- tasks/models.py
+++ tasks/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+from django.conf import settings
 
 class TaskManager(models.Manager):
     def get_queryset(self):
@@ -8,7 +8,6 @@ class TaskManager(models.Manager):
 
 
 class Task(models.Model):
-
     created_user = models.CharField(max_length=20)
     title = models.CharField(max_length=20)
     content = models.TextField(default="")
@@ -18,6 +17,8 @@ class Task(models.Model):
     end_time = models.DateTimeField(null=True)
     deleted_at = models.DateTimeField(null=True)
     level = models.CharField(max_length=1, default="")
+    situations_m2m = models.ManyToManyField("Situation", through="TakeTask")
+    member_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
 
     objects = TaskManager()
 
@@ -25,12 +26,13 @@ class Task(models.Model):
         self.deleted_at = timezone.now()
         self.save()
 
-
-class TakeTask(models.Model):
-    pass
-
-
 class Situation(models.Model):
     col_name = models.CharField(max_length=20, default="")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    tasks_m2m = models.ManyToManyField("Task", through="TakeTask")
+
+class TakeTask(models.Model):
+    task = models.ForeignKey("Task", on_delete=models.CASCADE, default=1)
+    situation = models.ForeignKey("Situation", on_delete=models.CASCADE, default=1)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
