46c76135e6f1970af5b0e912e83f186903f85a19
stephan9971 <tom9876555@yahoo.com.tw>
2024-05-18 00:48:34 +0800

    fix: project/settings.py CHANNEL_LAYERS hosts 改為環境變數。 chats/routing.py 修改路由名稱。 chats/model.py 新增一個私人聊天室 table 修改私人訊息外鍵名稱

--- chats/models.py
+++ chats/models.py
@@ -19,9 +19,14 @@ class GroupMessage(models.Model):
     class Meta:
         ordering = ['-created_at']
 
+class PrivateChatRoom(models.Model):
+    room_name = models.CharField(max_length=128, unique = True)
+
+    def __str__(self):
+        return self.group_name        
+
 class PrivateMessage(models.Model):
-    group = models.ForeignKey(ChatGroup, on_delete = models.CASCADE, related_name = 'private_messages',
-    )
+    room = models.ForeignKey(PrivateChatRoom, on_delete = models.CASCADE, related_name = 'private_messages')
     sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name ='sent_messages')
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name ='received_messages')
     content = models.TextField()
--- project/settings.py
+++ project/settings.py
@@ -119,7 +119,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [(os.getenv("CHANNEL_LAYERS_URL"), os.getenv("CHANNEL_LAYERS_PORT"))],
         },
     },
 }
