680b91621029a8fc4a95ca3a327a34f90e4f5147
stephan9971 <tom9876555@yahoo.com.tw>
2024-05-21 00:44:17 +0800

    fix: 解衝突並把 task modle bug 修正，add 產生新的 taks,member migration 檔

--- members/models.py
+++ members/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
+from tasks.models import Task, MemberTask
 
 
 class Member(AbstractUser):
