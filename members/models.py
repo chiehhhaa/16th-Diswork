902b42c46906665171c78878320f98b67ced7868
chiehhhaa <tomato870208@gmail.com>
2024-05-20 15:08:16 +0800

    fix: member table update

--- members/models.py
+++ members/models.py
@@ -4,8 +4,11 @@ from django.contrib.auth.models import AbstractUser
 
 class Member(AbstractUser):
     name = models.CharField(max_length=100, default="")
-    vip_user = models.CharField(max_length=50, default="")
-    vip_date_expiry = models.DateTimeField(null=True, blank=True)
+    status = models.CharField(max_length=50, default="")
     user_img = models.ImageField(null=True, blank=True)
     email = models.EmailField(unique=True)
     tasks = models.ManyToManyField("Task", through="MemberTask")
+
+
+class Status(models.Model):
+    plan = models.CharField(max_length=20, null=True, blank=True)
