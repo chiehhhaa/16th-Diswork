0decb808c636fb047625090e5b87f82cee651bce
chiehhhaa <tomato870208@gmail.com>
2024-05-22 16:22:01 +0800

    wit: boards

---
+++ boards/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.utils import timezone
+
+
+class CategoryManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted_at=None)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    rule = models.TextField()
+    status = models.CharField(
+        max_length=20,
+        choices=[
+            ("啟用", "啟用"),
+            ("禁用", "禁用"),
+        ],
+        default="啟用",
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
+
+    def deleted(self):
+        self.deleted_at = timezone.now()
+        self.save()
