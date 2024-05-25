92ef14cd6d9aa47704343679a8912af6aa8edf12
ErJun Wu <defyto13579@gmail.com>
2024-05-25 19:39:08 +0800

    fix: clean note

--- static/scripts/custom.js
+++ static/scripts/custom.js
@@ -1,17 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     let carousel = document.querySelector('.aaa');
-//     if (carousel) {
-//         let options = {
-//             infinite: true,
-//             slidesToShow: 3,
-//             slidesToScroll: 1,
-//             autoplay: true,
-//             autoplaySpeed: 1000,
-//         };
-//         new Slick(carousel, options);
-//     }
-// });
-
 $('.slick-carousel').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
