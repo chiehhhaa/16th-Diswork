e11a5d8802ab9d4b09bce9e80d84bdb066315f0d
stephan9971 <tom9876555@yahoo.com.tw>
2024-05-25 11:43:57 +0800

    fix: 刪除 members/forms 註解的 code

--- members/forms.py
+++ members/forms.py
@@ -27,30 +27,6 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = Member
-        field = ("name", "username", "email", "password", "password2", "user_img")
-    # name = forms.CharField(
-    #     label="姓名",
-    #     widget=forms.TextInput(attrs={"class": "mx-1 border-2 border-gray-300"}),
-    # )
-    # username = forms.CharField(
-    #     label="帳號",
-    #     widget=forms.TextInput(attrs={"class": "mx-1 border-2 border-gray-300"}),
-    # )
-    # email = forms.EmailField(
-    #     label="信箱",
-    #     widget=forms.EmailInput(attrs={"class": "mx-1 border-2 border-gray-300"}),
-    # )
-    # password1 = forms.CharField(
-    #     label="密碼",
-    #     widget=forms.PasswordInput(attrs={"class": "mx-1 border-2 border-gray-300"}),
-    # )
-    # password2 = forms.CharField(
-    #     label="密碼確認",
-    #     widget=forms.PasswordInput(attrs={"class": "mx-1 border-2 border-gray-300"}),
-    # )
-
-    class Meta:
-        model = Member
         fields = ("name", "username", "email", "password1", "password2", "user_img")
 
     def clean_email(self):
