55ec9715865d04ccb503d89e2a67215699fee5d5
stephan9971 <tom9876555@yahoo.com.tw>
2024-05-28 00:22:52 +0800

    新增留言按讚總數

--- articles/views.py
+++ articles/views.py
@@ -42,7 +42,7 @@ class ShowView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["comments"] = self.object.comments.all().order_by("-id")
+        context["comments"] = self.object.comments.annotate(like_count=Count("like_comment"))
         return context
 
     def post(self, request, pk):
