0e63fba30cead15b6bfdde1034653ec09b4f8c44
stephan9971 <tom9876555@yahoo.com.tw>
2024-05-27 11:59:50 +0800

    fix: 修正進入某一篇文章報錯

--- articles/views.py
+++ articles/views.py
@@ -34,7 +34,7 @@ class ShowView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["comments"] = self.object.comment_set.all().order_by("-id")
+        context["comments"] = self.object.comments.all().order_by("-id")
         return context
 
     def post(self, request, pk):
