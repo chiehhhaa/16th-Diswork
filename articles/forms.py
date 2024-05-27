ae406ed528c2577d4cf3c474744f4c344f26c360
ErJun Wu <defyto13579@gmail.com>
2024-05-28 01:57:12 +0800

    fix articles/new.html layouts

--- articles/forms.py
+++ articles/forms.py
@@ -6,21 +6,26 @@ from .models import Article
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = ["title", "author", "content"]
+        fields = ["title", "author", "content", "category"]
         labels = {
             "title": "標題",
             "author": "作者",
             "content": "內容",
+            "category": "分類",
         }
         widgets = {
             "title": forms.TextInput(
                 attrs={
-                    "class": "block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:border-gray-400 dark:bg-gray-200 dark:text-gray-800 dark:placeholder-gray-400"
+                    "class": "border border-gray-300 text-black rounded-md px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-gray-600"
                 },
             ),
             "content": forms.Textarea(
                 attrs={
-                    "class": "block w-full rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 dark:border-gray-400 dark:bg-gray-200 dark:text-gray-800 dark:placeholder-gray-400"
+                    "class": "border border-gray-300 text-black rounded-md px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-gray-600"
                 },
             ),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['author'].widget.attrs['readonly'] = True
