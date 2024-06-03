0cde81c199446d0f21a5a8065f8357281ed0fb36
stephan9971 <tom9876555@yahoo.com.tw>
2024-06-03 17:50:23 +0800

    feat: articles/models.py 新增一個 picture 欄位, 修改 articles/form.py 增加 picture 表單欄位

--- articles/models.py
+++ articles/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 from boards.models import Category
-
+from PIL import Image
+from io import BytesIO
+from django.core.files.base import ContentFile
 
 class ArticleManager(models.Manager):
     def get_queryset(self):
@@ -36,6 +38,7 @@ class Article(models.Model):
         settings.AUTH_USER_MODEL, through="LikeArticle", related_name="like_article"
     )
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    picture = models.ImageField(null=True, blank=True, upload_to="images/")
 
     objects = ArticleManager()
 
@@ -46,6 +49,19 @@ class Article(models.Model):
         self.deleted_at = timezone.now()
         self.save()
 
+    def save(self, *args, **kwargs):
+        if self.picture:
+            img = Image.open(self.picture)
+            max_size = (300, 300)
+            img.thumbnail(max_size, Image.LANCZOS)
+            thumb_io = BytesIO()
+            img_format = "PNG" if img.mode == "RGBA" else "JPEG"
+            img.save(thumb_io, format=img_format)
+            thumb_io.seek(0)
+            file_extension = "png" if img.mode == "RGBA" else "jpg"
+            new_file_name = f"{self.picture.name.split(".")[0]}_thumb.{file_extension}"
+            self.picture.save(new_file_name, ContentFile(thumb_io.read()), save=False)
+        super().save(*args, **kwargs)
 
 class LikeArticle(models.Model):
     like_article = models.ForeignKey(
