a917b01fe25edfd5f8e747babbaf1a7e658b8bdf
ErJun Wu <defyto13579@gmail.com>
2024-06-04 14:03:00 +0800

    fix urls

--- paies/urls.py
+++ paies/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'paies'
 
 urlpatterns = [
-    path('createOrder', views.create_order, name='create_order'),
+    path('create_order', views.create_order, name='create_order'),
     path('check/<int:TimeStamp>', views.check_order, name='check_order'),
     path('newebpays_return', views.newebpay_return, name='newebpay_return'),
     path('newebpays_notify', views.newebpay_notify, name='newebpay_notify'),
