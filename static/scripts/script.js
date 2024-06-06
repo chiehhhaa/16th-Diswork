c57b3908524d9aff69cce056b84fdd9c62589c12
ErJun Wu <defyto13579@gmail.com>
2024-06-06 11:03:46 +0800

    fix style

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,12 +1,35 @@
-$(function() {
-    $('[id^="category_"]').click(function() {
-        $('[id^="category_"]').removeClass('active')
-        $(this).addClass('active')
-        localStorage.setItem('activeLink', $(this).attr('id'));
+// $(function() {
+//     $('[id^="category_"]').click(function() {
+//         $('[id^="category_"]').removeClass('active')
+//         $(this).addClass('active')
+//         localStorage.setItem('activeLink', $(this).attr('id'));
+//     });
+//     const activeLinkId = localStorage.getItem('activeLink');
+//     console.log(activeLinkId);
+//     if (activeLinkId) {
+//         $('#' + activeLinkId).addClass('active');
+//     }
+// })
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    let buttons = document.querySelectorAll('[id^="category_"]');
+
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            buttons.forEach(function(btn) {
+                btn.classList.remove('active');
+            });
+
+            this.classList.add('active');
+
+            localStorage.setItem('activeLink', this.id);
+        });
     });
-    const activeLinkId = localStorage.getItem('activeLink');
+
+    let activeLinkId = localStorage.getItem('activeLink');
     console.log(activeLinkId);
     if (activeLinkId) {
-        $('#' + activeLinkId).addClass('active');
+        document.getElementById(activeLinkId).classList.add('active');
     }
-})
+});
