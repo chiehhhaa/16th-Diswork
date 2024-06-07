afa3d4767f4af9139186cbf5c81689964de6c616
ErJun Wu <defyto13579@gmail.com>
2024-06-07 18:16:59 +0800

    fix: 討論版不換頁

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,19 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let buttons = document.querySelectorAll('[id^="category_"]');
+// document.addEventListener('DOMContentLoaded', function() {
+//     let buttons = document.querySelectorAll('[id^="category_"]');
 
-    buttons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            buttons.forEach(function(btn) {
-                btn.classList.remove('active');
-            });
+//     buttons.forEach(function(button) {
+//         button.addEventListener('click', function() {
+//             buttons.forEach(function(btn) {
+//                 btn.classList.remove('active');
+//             });
 
-            this.classList.add('active');
+//             this.classList.add('active');
 
-            localStorage.setItem('activeLink', this.id);
-        });
-    });
-    const activeLinkId = localStorage.getItem('activeLink');
-    if (activeLinkId) {
-        document.getElementById(activeLinkId).classList.add('active');
-    }
-});
+//             localStorage.setItem('activeLink', this.id);
+//         });
+//     });
+
+//     let activeLinkId = localStorage.getItem('activeLink');
+//     console.log(activeLinkId);
+//     if (activeLinkId) {
+//         document.getElementById(activeLinkId).classList.add('active');
+//     }
+// });
