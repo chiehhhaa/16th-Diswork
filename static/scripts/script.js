71bfa73934192bcb9e84b27391db60925081958a
ErJun Wu <defyto13579@gmail.com>
2024-06-08 19:39:46 +0800

    fix 讓多個輪播運行

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,48 +1,29 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     let buttons = document.querySelectorAll('[id^="category_"]');
+function setupCarousel(carousel) {
 
-//     buttons.forEach(function(button) {
-//         button.addEventListener('click', function() {
-//             buttons.forEach(function(btn) {
-//                 btn.classList.remove('active');
-//             });
-
-//             this.classList.add('active');
-
-//             localStorage.setItem('activeLink', this.id);
-//         });
-//     });
-
-//     let activeLinkId = localStorage.getItem('activeLink');
-//     console.log(activeLinkId);
-//     if (activeLinkId) {
-//         document.getElementById(activeLinkId).classList.add('active');
-//     }
-// });
-
-const carousel = document.querySelector(".ads-carousel")
-const slides = carousel.querySelectorAll(".slide")
-const track = carousel.querySelector(".track")
-console.log(slides.length)
-function setupSlides() {
-    const w = track.clientWidth
-
-    slides.forEach((slide, i) => {
-        slide.style.left = `${i * w}px`
-    })
-    adsInterval(0, w)
-}
-
-function adsInterval(index, w) {
-    let slideIndex = index;
+    const slides = carousel.querySelectorAll(".slide")
+    const track = carousel.querySelector(".track")
+    console.log(slides.length)
+    function setupSlides() {
+        const w = track.clientWidth
     
-    setInterval(function() {
-        slideIndex = (slideIndex + 1) % slides.length;
-        track.style.transform = `translateX(-${slideIndex * w}px)`
-    }, 3000)
-    console.log("test")
+        slides.forEach((slide, i) => {
+            slide.style.left = `${i * w}px`
+        })
+        adsInterval(0, w)
+    }
+    
+    function adsInterval(index, w) {
+        let slideIndex = index;
+        
+        setInterval(function() {
+            slideIndex = (slideIndex + 1) % slides.length;
+            track.style.transform = `translateX(-${slideIndex * w}px)`
+        }, 3000)
+        console.log("test")
+    }
+    setupSlides()
 }
 
-setupSlides()
+document.querySelectorAll(".ads-carousel").forEach(setupCarousel)
 
 
