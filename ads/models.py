45beb6d9292a7cee509e3f6c2e8af8e7ed8d0f17
Jeter Chen <dali1756@gmail.com>
2024-06-08 19:11:45 +0800

    fix:調整title欄位屬性

--- ads/models.py
+++ ads/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from lib.softdelete import SoftDeleteable
 
 class Ads(SoftDeleteable, models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=255)
     picture = models.ImageField(upload_to='ads/', verbose_name="picture")
     url = models.URLField(max_length=200, verbose_name="URL")
     created_at = models.DateTimeField(auto_now_add=True)
