3f58121885ad5d70b4d0970057d045e2910ef02b
chiehhhaa <tomato870208@gmail.com>
2024-06-11 22:30:18 +0800

    Modify: 訊息頁分頁樣式

--- chats/views.py
+++ chats/views.py
@@ -8,19 +8,23 @@ from members.models import Member
 from friends.models import Friend
 from boards.models import Category
 from django.db.models import Q, Prefetch, OuterRef, Subquery
+from django.core.paginator import Paginator
+
 
 @login_required
 def chat_home(request):
     chat_groups = ChatGroup.objects.all()
     return render(request, "chats/chat_home.html", {"chat_groups": chat_groups})
 
+
 @login_required
 def chat_new(request):
     return render(request, "chats/chat_create.html")
 
+
 @login_required
 def chat_show(request, id):
-    chat_group = get_object_or_404(ChatGroup, pk = id)
+    chat_group = get_object_or_404(ChatGroup, pk=id)
     chat_messages = chat_group.chat_messages.all()[:30]
     form = ChatmessageCreateForm()
 
@@ -31,36 +35,55 @@ def chat_show(request, id):
             message.author = request.user
             message.group = chat_group
             message.save()
-            return redirect('chats:show', id=chat_group.id)
-    return render(request, "chats/chat_group.html", {'chat_messages': chat_messages, 'form': form})
+            return redirect("chats:show", id=chat_group.id)
+    return render(
+        request, "chats/chat_group.html", {"chat_messages": chat_messages, "form": form}
+    )
+
 
 @login_required
 def chat_create(request):
-    if request.method == 'POST':
-        group_name = request.POST['group_name']
-        if not ChatGroup.objects.filter(group_name = group_name).exists():
-            chat_group = ChatGroup(group_name = group_name)
-            messages.success(request, '新增成功')
+    if request.method == "POST":
+        group_name = request.POST["group_name"]
+        if not ChatGroup.objects.filter(group_name=group_name).exists():
+            chat_group = ChatGroup(group_name=group_name)
+            messages.success(request, "新增成功")
             chat_group.save()
         else:
-            messages.error(request, '群組名稱重複請更換名稱')
-    return redirect('chats:home')
+            messages.error(request, "群組名稱重複請更換名稱")
+    return redirect("chats:home")
+
 
 @login_required
 def private_message_home(request):
-    category_list = Category.objects.all()
+    latest_messages_subquery = (
+        PrivateMessage.objects.filter(
+            receiver_id=OuterRef("receiver_id"), sender_id=OuterRef("sender_id")
+        )
+        .order_by("-created_at")
+        .values("created_at")[:1]
+    )
 
-    latest_messages_subquery = PrivateMessage.objects.filter(
-        receiver_id=OuterRef('receiver_id'),
-        sender_id=OuterRef('sender_id')
-    ).order_by('-created_at').values('created_at')[:1]
+    latest_messages = (
+        PrivateMessage.objects.filter(
+            receiver_id=request.user.id, created_at=Subquery(latest_messages_subquery)
+        )
+        .select_related("sender", "private_room")
+        .order_by("-created_at")
+    )
 
-    latest_messages = PrivateMessage.objects.filter(
-        receiver_id=request.user.id,
-        created_at=Subquery(latest_messages_subquery)
-    ).select_related("sender", "private_room").order_by("-created_at")
-    
-    return render(request, "chats/private_message_home.html", {"private_messages": latest_messages, "category_list": category_list})
+    paginator = Paginator(latest_messages, 5)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        "chats/private_message_home.html",
+        {
+            "private_messages": latest_messages,
+            "page_obj": page_obj,
+        },
+    )
 
 
 @login_required
@@ -68,22 +91,55 @@ def private_message_room(request, room_name):
     privates_users = room_name.split("_")
     category_list = Category.objects.all()
 
-    if int(privates_users[0]) == request.user.id or int(privates_users[1]) == request.user.id:
-        check_private_room = PrivateChatRoom.objects.filter(room_name=room_name).exists()
+    if (
+        int(privates_users[0]) == request.user.id
+        or int(privates_users[1]) == request.user.id
+    ):
+        check_private_room = PrivateChatRoom.objects.filter(
+            room_name=room_name
+        ).exists()
         if check_private_room:
             private_room = PrivateChatRoom.objects.prefetch_related(
-            Prefetch('private_messages', queryset=PrivateMessage.objects.select_related('sender', 'receiver'))).get(room_name=room_name)
-            receiver_id = next(user_id for user_id in privates_users if request.user.id != int(user_id))
+                Prefetch(
+                    "private_messages",
+                    queryset=PrivateMessage.objects.select_related(
+                        "sender", "receiver"
+                    ),
+                )
+            ).get(room_name=room_name)
+            receiver_id = next(
+                user_id for user_id in privates_users if request.user.id != int(user_id)
+            )
             receiver = Member.objects.get(id=receiver_id)
             private_messages = private_room.private_messages.all()
 
-            return render(request, "chats/private_message_room.html", {"room_name": room_name, "private_messages": private_messages, "receiver": receiver, "category_list": category_list})
-        
+            return render(
+                request,
+                "chats/private_message_room.html",
+                {
+                    "room_name": room_name,
+                    "private_messages": private_messages,
+                    "receiver": receiver,
+                },
+            )
+
         else:
-            receiver_id = next(user_id for user_id in privates_users if request.user.id != int(user_id))
+            receiver_id = next(
+                user_id for user_id in privates_users if request.user.id != int(user_id)
+            )
             receiver = Member.objects.get(id=receiver_id)
-            return render(request, "chats/private_message_room.html", {"room_name": room_name, "receiver": receiver})
+            return render(
+                request,
+                "chats/private_message_room.html",
+                {"room_name": room_name, "receiver": receiver},
+            )
     else:
-        friend_list = Friend.objects.filter(Q(receiver_id=request.user.id) | Q(sender_id=request.user.id) & ~Q(receiver_id=request.user.id) & ~Q(sender_id=request.user.id) & Q(status=2))
-        messages.error(request, '操作錯誤')
+        friend_list = Friend.objects.filter(
+            Q(receiver_id=request.user.id)
+            | Q(sender_id=request.user.id)
+            & ~Q(receiver_id=request.user.id)
+            & ~Q(sender_id=request.user.id)
+            & Q(status=2)
+        )
+        messages.error(request, "操作錯誤")
         return render(request, "friends/friend_list.html", {"friend_list": friend_list})
